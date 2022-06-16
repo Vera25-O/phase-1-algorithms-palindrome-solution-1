177cd25b65b862401c6376c27bd0f33838c582ac
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-16 15:47:30 +0300

    commit tree 1fc82a8eae223dd57c7aa51c4c155d1ae8a9d558

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  function reverseString(word) {
+    
+    const wordArray = word.split("");
+   
+    const reversedWordArray = wordArray.reverse();
+    
+    const reversedWord = reversedWordArray.join("");
+    
+    return reversedWord;
+  }
+   
+   const reversedWord = reverseString(word);
+   
+   return word === reversedWord;
+  
 }
-
 /* 
+create an array from the input string
+ reverse the array
+ create a string from the reversed array
+ return the reversed string
+ reverse the input string
+compare the reversed string to the input
+ compare the reversed string to the input
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  applied a problem solving process to one specific algorithm problem.
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +44,32 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
